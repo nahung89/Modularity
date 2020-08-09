e57cb42ce9c86ccee3f78188e51ad8cf7314a99b
Nah <nahung89@gmail.com>
2020-08-09 22:14:32 +0700

    1st init

---
+++ Application/Application/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Application
+//
+//  Created by Nah on 8/9/20.
+//  Copyright © 2020 Nah. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ Application/Application/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Application
+//
+//  Created by Nah on 8/9/20.
+//  Copyright © 2020 Nah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
